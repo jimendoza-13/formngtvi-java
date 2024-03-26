ed0417bd2481930fef93a90cf004ccc87897f2c0
johnjoseph.mendoza <132543006+jimendoza-13@users.noreply.github.com>
2024-03-26 09:58:37 +0000

    Output is Done

--- ArithmeticTask.java
+++ ArithmeticTask.java
@@ -1,1 +1,24 @@
+public class ArithmeticTask {
 
+    public static void main (String[] args){
+          
+        int result = 1 + 2; // result is now 3
+        System.out.println(result);
+
+        result -= result - 1; // result is now 1
+        System.out.println(result);
+
+        result *= result * 2; // result is now 2
+        System.out.println(result);
+
+        result /= result / 2; // result is now 2
+        System.out.println(result);
+
+        result += result + 8; // result is now 12
+        System.out.println(result);
+
+        result %= result % 7; // result is now 2
+        System.out.println(result);
+
+    }
+}
