0e0406a227021832a9adc9446c6b89445dfb2024
johnjoseph.mendoza <132543006+jimendoza-13@users.noreply.github.com>
2024-03-27 10:00:19 +0000

    task-1-1-3 output is done

--- tasksheet_1_1_3.java
+++ tasksheet_1_1_3.java
@@ -1,1 +1,12 @@
+public class tasksheet_1_1_3 {
 
+    public static void main(String[] args) {
+        int check_number = 10;
+        String message;
+        
+        for (int i = 0; i <= check_number; i++){
+        message = (i % 2 == 0) ? i + " is even number" : i + " is odd number";
+        System.out.println(message);
+        }
+    }
+}
