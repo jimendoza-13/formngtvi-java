ca3c984a22f0ee54e2490e12ed8321a5c78f8e90
johnjoseph.mendoza <132543006+jimendoza-13@users.noreply.github.com>
2024-05-16 16:15:50 +0000

    Task 13 is Done

--- FileReader.java
+++ FileReader.java
@@ -18,6 +18,9 @@ class FileReader {
 }
 
 class FileReaderApp {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         String filename = "my_data.txt"; // Replace with your desired filename
         try {
